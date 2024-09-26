e51569f79bd40c17fe1778e8e3ee1c03c08a41d7
Suman Chakrabortty <suman.chakrabortty8514@gmail.com>
2024-09-26 19:15:26 +0530

    SumanOnlineStore

---
+++ connection.php
@@ -0,0 +1,4 @@
+<?php
+// $con=mysqli_connect("localhost","sumanonl_shop","sumanonl_shop","sumanonl_shop") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","root","","shop") or die(mysqli_error($con));
+?>
